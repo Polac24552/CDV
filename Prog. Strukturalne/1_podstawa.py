499335bc6fb198e38a427fd053dd6f0d19833e81
Jakub Połać <jpolac@edu.cdv.pl>
2020-03-04 15:27:19 +0100

    lesson1

--- Prog. Strukturalne/1_podstawa.py
+++ Prog. Strukturalne/1_podstawa.py
@@ -1,1 +1,37 @@
 print("CDV")
+print(10)
+'''
+komentarz blokowy
+'''
+#potegowanie komentarz jednej lini
+potega = 2 ** 10
+print (potega)
+
+#pobieranie danych z klawiatury
+name=input()
+nazwisko=input()
+print("Twoje imie: "+name)
+print("Twoje imie: "+name+", nazwisko: "+nazwisko)
+'''
+Uzytkownik podaje nazwy z klawiatury swój wiek,
+Twój wiek: ..lat
+'''
+print("podaj wiek: ", end="") #nie przechodzi do nowej lini
+wiek=input()
+print(type(wiek))
+print("Twoj wiek: ",wiek," lat")
+
+age1=20
+print(type(age1))
+
+surname="Kowalski"
+firstLetter=surname[0]
+print(firstLetter)
+
+length=len(surname)
+print(length)
+
+lastLetter=surname[len(surname)-1]
+print(lastLetter)
+
+#konwersja 
